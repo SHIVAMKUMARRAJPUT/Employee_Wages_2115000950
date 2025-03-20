49ef73af3bfcedea6c75c37cd09a03aa8d54bfcc
unknown <shivamrajput81202@gmail.com>
2025-03-20 23:28:07 +0530

    [Shivam] Add :Add new UC1 codes

---
+++ EmployeeWage.js
@@ -0,0 +1,10 @@
+// UC1 - Check Employee Presence
+
+const IS_PRESENT = 1;
+let empCheck = Math.floor(Math.random() * 2);
+
+if (empCheck === IS_PRESENT) {
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is Absent");
+}
